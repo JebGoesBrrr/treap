57f1d3dbd66e045f3a6cb3347b66d74a81900239
Alexander Schneiders <alexanderschneiders@protonmail.com>
2022-03-23 10:47:39 +0100

    add doc

--- include/treap.h
+++ include/treap.h
@@ -12,10 +12,18 @@ typedef void* (treap_mapfunc)(const void*);
  * @brief   Treap (tree + heap) data structure.
  * @details A treap stores an ordered set of keys (and optionally associated values)
  *          and has high probability to have O(log n) height.
+ *          Each node stores a key (and optionally associated value) as well as a random
+ *          priority. Nodes must respect the search-tree property w.r.t the keys and the
+ *          heap property w.r.t the priority. Randomizing priorities gives a high
+ *          probability for the tree to be balanced.
  */
 struct treap;
 
-struct treap_kvpair;
+struct treap_kvpair {
+    int isvaild;
+    const void* key;
+    void *value;
+};
 
 
 /**
@@ -36,31 +44,27 @@ extern struct treap* treap_create(treap_cmpfunc *cmpfunc, int ksize, int vsize);
 /**
  * 
  */
-extern struct treap treap_duplicate(const struct treap *self);
+extern struct treap* treap_duplicate(const struct treap *self);
 
 
 /**
- * @brief   Destroy a treap.
- * @details Free all memory associated with a treap.
+ * @brief     Destroy a treap.
+ * @details   Free all memory associated with a treap.
  * @param[in] self The treap to the destroyed.
  */
 extern void treap_destroy(struct treap *self);
 
 
 /**
- *
+ * @brief     Retrieve the number of keys in the treap
+ * @param[in] self The treap who's number of keys is retrieved.
+ * @return         The number of keys in the treap.
  */
 extern int treap_count(const struct treap *self);
 
 
 /**
- * 
- */
-extern int treap_datasize(const struct treap *self);
-
-
-/**
- * @brief Insert a new key into a treap.
+ * @brief     Insert a new key into a treap.
  * @param[in] self  The treap to be updated.
  * @param[in] key   Pointer to the key that is inserted.
  * @param[in] val   Pointer to the value that is inserted.
@@ -69,7 +73,7 @@ extern void treap_insert(struct treap *self, const void *key, const void *val);
 
 
 /**
- * @brief Remove a key from a treap.
+ * @brief     Remove a key from a treap.
  * @param[in] self  The treap to be updated.
  * @param[in] key   Pointer to the key that is to be remvoed.
  * @return          1 if \p key was removed, and 0 if \p key was not found.
@@ -78,9 +82,14 @@ extern int treap_remove(struct treap *self, const void *key);
 
 
 /**
- * 
+ * @brief     Look up a key in the treap.
+ * @param[in] self The treap in which to look for \p key
+ * @param[in] key  The key to be looked up.
+ * @return         Returns a key-value pair. If the key is not found, \c isvalid will
+ *                 be zero, and non zero otherwise. If the treap only stores keys, but
+ *                 not values, the \c value field will be \c NULL
  */
-extern const struct treap_kvpair* treap_lookup(const struct treap *self, const void *key);
+extern struct treap_kvpair treap_lookup(const struct treap *self, const void *key);
 
 
 /**
